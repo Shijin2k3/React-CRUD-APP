6e2fe29066a5b6f8aeae12aa74c45000e21a3eb2
Shijin2k3 <shijinkumar2003@gmail.com>
2025-05-02 15:07:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import {BrowserRouter,Routes,Route, Router} from 'react-router-dom'
+import './App.css'
+import Home from './Home'
+import Create from './Create'
+import Update from './Update'
+import Read from './Read'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/create' element={<Create/>}/>
+      <Route path='/update/:id' element={<Update/>}/>
+      <Route path='/read/:id' element={<Read/>}/>
+    </Routes>
+   </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='d-flex  flex-column justify-content-center align-items-center bg-light vh-100'>
+        <h1>List of Users</h1>
+        <div className='w-75 rounded bg-white border shadow p-4'></div>
+    </div>
+  )
+}
+
+export default Home
